b3f2b15b373e9197e0756009dc61d8d7b0f00f77
duncan fedde <duncan.fedde@ensercasolutions.com>
2014-03-07 09:01:54 -0700

    changed exports to work in linux

--- home/.zsh/exports.zsh
+++ home/.zsh/exports.zsh
@@ -1,9 +1,5 @@
 # Currently this path is appendend to dynamically when picking a ruby version
-export PATH=bin:script:~/.rbenv/bin:~/.bin:node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/local/share/npm/bin:/opt/boxen/nvm/v0.8.8/bin:$PATH
-
-# Set default console Java to 1.6
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_13.jdk/Contents/Home
-export ANDROID_SDK_ROOT=/opt/boxen/homebrew/opt/android-sdk
+export PATH=bin:script:~/.rbenv/bin:~/.bin:node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/local/share/npm/bin:$PATH
 
 # Setup terminal, and turn on colors
 export TERM=xterm-256color
@@ -19,8 +15,7 @@ export ARCHFLAGS='-arch x86_64'
 
 export LESS='--ignore-case --raw-control-chars'
 export PAGER='less'
-export EDITOR='/Applications/MacVim.app/Contents/MacOS/Vim'
-export NODE_PATH=/opt/boxen/nodenv/versions/v0.10/lib
+export EDITOR='/usr/bin/vim'
 export PYTHONPATH=/usr/local/lib/python2.6/site-packages
 # CTAGS Sorting in VIM/Emacs is better behaved with this in place
 export LC_COLLATE=C
